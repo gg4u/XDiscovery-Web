89dc79668b17ca2bdbbf03d1c7a17316eb648b10
Marco Paolini <marco@credra.com>
2013-10-08 20:07:27 +0200

    Do not use trailing slash on REST API uris.

--- xdimension_web/xdw_core/urls.py
+++ xdimension_web/xdw_core/urls.py
@@ -8,6 +8,6 @@ from .views.maps import MapDetail, MapList
 # API entry point
 urlpatterns = patterns(
     '',
-    url(r'^map/$', MapList.as_view(), name='map-list'),
-    url(r'^map/(?P<pk>\d+)/$', MapDetail.as_view(), name='map-detail'),
+    url(r'^map$', MapList.as_view(), name='map-list'),
+    url(r'^map/(?P<pk>\d+)$', MapDetail.as_view(), name='map-detail'),
     )
