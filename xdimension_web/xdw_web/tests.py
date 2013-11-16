9b6d07d04d599dae49c47fe68ff0ef283e1756f6
Marco Paolini <marco@credra.com>
2013-11-17 00:42:03 +0100

    Tests for graph detail page.

---
+++ xdimension_web/xdw_web/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from xdimension_web.xdw_core.tests.test_utils import MapTestCaseMixIn
+
+
+class WebTestCase(TestCase, MapTestCaseMixIn):
+
+    def test_no_map(self):
+        resp = self.client.get('/en/atlas/12/')
+        self.assertEqual(resp.status_code, 404)
+
+    def test_map(self):
+        map_ = self.create_maps(1)[0]
+        resp = self.client.get('/en/atlas/{}'.format(map_.pk))
+        self.assertEqual(resp.status_code, 200)
+        print resp.content
