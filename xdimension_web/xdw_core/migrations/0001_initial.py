9cd94de252b0fba748905d6c17e6330207aeac29
Marco Paolini <marco@credra.com>
2013-12-12 00:03:01 +0100

    Fix migration of apps relatively to cms apps.

--- xdimension_web/xdw_core/migrations/0001_initial.py
+++ xdimension_web/xdw_core/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('cms', '0001_initial.py'),
+    )
+
     def forwards(self, orm):
         # Adding model 'Map'
         db.create_table(u'xdw_core_map', (
