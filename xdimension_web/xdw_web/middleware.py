16eb955c29aa978a68e2395636ea94d38e8d05bf
Marco Paolini <marco@credra.com>
2014-04-02 17:18:59 +0200

    Allow display of atlas app in WIP mode.

--- xdimension_web/xdw_web/middleware.py
+++ xdimension_web/xdw_web/middleware.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
 
+OK_URLS = ['/en/atlas', '/api', '/views', '/map', '/graph']
+
+
 class WIPMiddleware(object):
 
     def process_request(self, request):
         if (not request.user.is_authenticated() and
-                not request.path.startswith(reverse('admin:index'))):
+                not request.path.startswith(reverse('admin:index')) and
+                not [None for x in OK_URLS if request.path.startswith(x)] and
+                not request.GET.get('angular') and
+                not request.is_ajax()):
             return render(request, 'xdw_web/wip.html')
