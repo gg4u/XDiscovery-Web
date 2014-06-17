6b36bf33a94dc214b9dd8e3db73446a86bc05a7a
Marco Paolini <marco@credra.com>
2014-06-17 16:28:20 +0200

    More on FB and Twitter sharing

--- xdimension_web/xdw_web/opengraph.py
+++ xdimension_web/xdw_web/opengraph.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 def get_opengraph_context():
     env = settings.DEPLOY_MODE
-    # TODO: enable this when fb app goes live
     return {
         #'site:name': settings.FB_SITE_NAME,
-        #'fb:app_id': settings.FB_APP_ID[env]
+        'fb:app_id': settings.FB_APP_ID[env],
+        "fb:admins": "1385024622",
+        "author": "477638342277879",
+        "publisher": "205040896287846",
     }
--- xdimension_web/xdw_web/views.py
+++ xdimension_web/xdw_web/views.py
@@ -1,4 +1,8 @@
+# coding: utf8
+
 from __future__ import absolute_import
+
+import logging
 
 from django.views.generic.base import View
 from django.http import HttpResponse
@@ -9,6 +13,8 @@ from django.conf import settings
 
 from xdimension_web.xdw_core.models import Map
 from .opengraph import get_opengraph_context
+
+logger = logging.getLogger(__name__)
 
 
 class RobotsView(View):
@@ -31,31 +37,53 @@ class AtlasView(View):
 
 
 class GraphDetailView(View):
+
     def get(self, request, pk, language=None):
         og_context = get_opengraph_context()
         map_ = get_object_or_404(Map, pk=pk, status=Map.STATUS_OK)
         site = get_current_site(request)
-        
-        def get_description(max_length, hashtags=False):
-            desc, desc_len = [], 0
-            for title in map_.node_titles:
-                if hashtags and ' ' not in title:
+        map_url = '{}://{}{}'.format(
+                settings.SHARING_PROTO,
+                site.domain,
+                reverse('graph_detail', args=[settings.LANGUAGE_CODE, pk]))
+        desc_fmt_1 = (
+            u'A map about: {{topics}} and other #{{more_topics}} topics! '
+            u'{url} |'
+            u'Get Human Knowledge in your hands > '
+            u'#LearnDiscovery app {app_url}')
+        # TODO: confirm app url
+        desc_fmt = desc_fmt_1.format(url=map_url,
+                                     app_url='http://bit.ly/1pcHMZ3'
+        )
+
+        def possible_descriptions(max_length, hashtags=True):
+            desc = []
+            sep = u' · '
+            for title in map_.node_titles[:3]:
+                if hashtags:
+                    title = title.replace(' ', '')
                     title = u'#{}'.format(title)
-                desc_len += len(title)
-                if desc_len > 200 and desc:
-                    break
                 desc.append(title)
-            return u' '.join(desc)
+            yield desc_fmt.format(
+                topics=sep.join(desc),
+                more_topics=len(map_.node_titles) - len(desc)
+            )
+            yield sep.join(desc)
+            yield desc[0]
+
+        def get_description(max_length, **kwargs):
+            for description in possible_descriptions(max_length, **kwargs):
+                if len(description) <= max_length:
+                    return description
+                logger.warning('description too long for map {}'.format(map_.pk))
+            logger.error('can\'t set description for map {}'.format(map_.pk))
 
         # Facebook
         og_context.update({
             'og:title': map_.get_title(),
-            'og:url': '{}://{}{}'.format(
-                settings.SHARING_PROTO,
-                site.domain,
-                reverse('graph_detail', args=[settings.LANGUAGE_CODE, pk])),
+            'og:url': map_url,
             'og:image': map_.get_thumbnail_url(),
-            'og:description': get_description(200),
+            'og:description': get_description(500),
         })
 
         # Twitter
@@ -63,7 +91,7 @@ class GraphDetailView(View):
             'twitter:card': 'summary_large_image',
             #'twitter:site': xxx
             'twitter:title': map_.get_title(),
-            'twitter:description':  get_description(200, hashtags=True),
+            'twitter:description': get_description(200),
             #'twitter:creator': xxx
             'twitter:image:src': map_.get_thumbnail_url(),
             #'twitter:domain': XXX
