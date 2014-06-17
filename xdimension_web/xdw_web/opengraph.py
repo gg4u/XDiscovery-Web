0208d8071c9a77d8777cc2961ed94dddbad671a2
Marco Paolini <marco@credra.com>
2014-06-17 09:50:38 +0200

    Disable some useless fb OG meta tags.

--- xdimension_web/xdw_web/opengraph.py
+++ xdimension_web/xdw_web/opengraph.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 def get_opengraph_context():
     env = settings.DEPLOY_MODE
-    # TODO: includi anche "og:author" e og:publisher
+    # TODO: enable this when fb app goes live
     return {
-        'site:name': settings.FB_SITE_NAME,
-        'fb:app_id': settings.FB_APP_ID[env]
+        #'site:name': settings.FB_SITE_NAME,
+        #'fb:app_id': settings.FB_APP_ID[env]
     }
