ca68b4358ab02e8419bcb4c35a0976522f8ecdfa
Marco Paolini <marco@credra.com>
2014-10-27 11:33:02 +0100

    further fixes

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,14 +1,18 @@
 # To be run by the developer's own user
-export PYTHONUNBUFFERED="Y"
+FRONTEND_DIR=/vagrant/frontend
+BASE_DIR=/vagrant
+
+cd $HOME
 
 # python
-virtualenv virtual
-source virtual/bin/activate
-pip install -r /vagrant/requirements/dev.txt
+export PYTHONUNBUFFERED="Y"
+virtualenv $HOME/virtual
+source $HOME/virtual/bin/activate
+pip install -r $BASE_DIR/requirements/dev.txt
 
 # ruby
 curl -L get.rvm.io | bash -s stable
-source /home/vagrant/.rvm/scripts/rvm
+source $HOME/.rvm/scripts/rvm
 rvm install 1.9.3
 rvm use 1.9.3
 rvm gemset create xdiscovery_web
@@ -17,20 +21,24 @@ gem install compass --version=0.12.2
 
 # node
 curl https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | bash
-source /home/vagrant/.nvm/nvm.sh
+source $HOME/.nvm/nvm.sh
 nvm install 0.10.20
 nvm use 0.10.20
-pushd /vagrant/frontend
-rm -rf node_modules  # Just in case someone created it outside the VM
+# Dance to install node modules out of the shared directory
+cp $FRONTEND_DIR/package.json $HOME
 npm install -g grunt-cli bower
-npm install  # Installing into current folder is the only way to please grunt...
-popd
+npm install
+[ -f $FRONTEND_DIR/node_modules ] && mv $FRONTEND_DIR/node_modules $FRONTEND_DIR/node_modules_bak  # Just in case someone created it outside the VM
+ln -s $HOME/node_modules $FRONTEND_DIR/node_modules
+
+cd $BASE_DIR
 
 # Init db and rcfiles
-fab create_db_local:create_role=1 &&
-cat >> /home/vagrant/.bash_profile <<EOF
+cat >> $HOME/.bash_profile <<EOF
 cd /vagrant
 EOF
 
 # First build (includes a bower update run)
+fab create_db_local:create_role=1 &&
 fab dep
+fab build
